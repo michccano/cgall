e6db1fa3f9838e2cbc665ecde2f9a039d4dfb69e
unknown <mccano@protonmail.com>
2021-07-24 12:08:52 +0800

    FInished all screens & implemented backend functions

--- src/components/Upload_list.js
+++ src/components/Upload_list.js
@@ -3,9 +3,48 @@ import Button from "@material-ui/core/Button";
 import Preview_card from "./Preview_card";
 
 function Upload_list() {
+    function one_go(){
+        document.getElementById("upload_list").style.display="none";
+        document.getElementById("upload_list2").style.display="block";
+        loading_time();
+    }
+    function fewSec() {
+        document.getElementById("upload_list2").style.display = "none";
+    }
+    function fewSec2() {
+        document.getElementById("upload_list3").style.display = "block";
+    }
+    function fewSec3() {
+        document.getElementById("upload_list3").style.display = "none";
+        
+    }
+    function fewSec4(){
+        document.getElementById("upload_list4").style.display = "block";
+    }
+
+    function loading_time() {
+        setTimeout(() => {
+            fewSec();
+        }, 2000);
+
+        setTimeout(() => {
+            fewSec2();
+        }, 2000);
+
+        setTimeout(() => {
+            fewSec3();
+        }, 4000);
+
+        setTimeout(() => {
+            fewSec4();
+        }, 4000);
+        
+
+    }
+
     return (
         <>
-            <div className="upload_list">
+            <div className="upload_list" id="upload_list">
                 <div className="all_upload_info">
                     <div className="row">
                         <div className="col-lg-8">
@@ -144,6 +183,7 @@ function Upload_list() {
                     <Button
                         variant="contained"
                         className="font-weight-light mt-5 mb-5"
+                        onClick={one_go}
                         style={{
                             backgroundColor: "#4737FF",
                             color: "#fff",
@@ -160,6 +200,120 @@ function Upload_list() {
                     </Button>
                 </div>
             </div>
+        
+                {/* Waiting for confirmation...  */}
+            <div
+                className="upload_list2"
+                id="upload_list2"
+                style={{ display: "none" }}
+            >
+                <div className="all_upload_info">
+                    <div className="row">
+                        <div className="col-lg-8">
+                            <h1 className="fw-light mb-5">Waiting for confirmation...</h1>
+
+                            <p className="mb-5">
+                            To list this NFT, please confirm this transaction with your wallet.
+                            </p>
+                            <h6>Connected to</h6>
+                            <button style={{display: "flex", alignItems:"center", justifyContent:"center", backgroundColor:"#f4f4f4", width:"158px", height:"40px", borderRadius:"30px"}}><img className="mb-0" src="/img/green_btn.png" style={{width: "10px"}}/> <p className="mb-0 ms-2">0x01a...234N</p></button>
+
+                            <img
+                                    src="/img/loadline.png"
+                                    alt=""
+                                    style={{ maxWidth: "64px" }}
+                                    className="my-4 spining_load"
+                                />
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <h5 className="mt-3 mb-5">Preview</h5>
+                            <Preview_card />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+   {/* Your NFT is being listed...  */}
+   <div
+                className="upload_list3"
+                id="upload_list3"
+                style={{ display: "none" }}
+            >
+                <div className="all_upload_info">
+                    <div className="row">
+                        <div className="col-lg-8">
+                            <h1 className="fw-light mb-5">Your NFT is being listed...</h1>
+
+                            <p className="mb-5">
+                            Your NFT has been submitted to be listed in Market. You may leave this page if you like. It will be live as soon as the transaction is processed.
+
+                            </p>
+                            <a href="#" 
+                            style={{fontSize:"14px", color:"rgba(0,0,0,50%)", display:"block"}}><i class="fas fa-external-link-alt"></i> &nbsp;  View on Etherscan</a>
+                            <img
+                                    src="/img/loadline.png"
+                                    alt=""
+                                    style={{ maxWidth: "64px" }}
+                                    className="my-4 spining_load"
+                                />
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <h5 className="mt-3 mb-5">Preview</h5>
+                            <Preview_card />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+   {/*Your NFT is now listed!  */}
+   <div
+                className="upload_list4"
+                id="upload_list4"
+                style={{ display: "none" }}
+            >
+                <div className="all_upload_info">
+                    <div className="row">
+                        <div className="col-lg-8">
+                            <h1 className="fw-light mb-5">Your NFT is now listed!</h1>
+
+                            <p className="mb-5">
+                            Congratulations! Your artwork has been successfully listed in Market.
+
+                            </p>
+                        
+                            
+                            
+                            <Button
+                        variant="contained"
+                        className="fw-light my-3"
+                        
+                        style={{
+                            letterSpacing: "3px",
+                            backgroundColor: "rgb(71, 55, 255)",
+                            color: "#fff",
+                            width: "241px",
+                            height: "62px",
+                            borderRadius: 0,
+                            boxShadow: "rgb(163, 185, 255) 0px 0px 1px 5px",
+                        }}
+                    >
+                        View Your NFT
+                    </Button>
+
+                    <a href="#" 
+                            style={{fontSize:"14px", color:"rgba(0,0,0,50%)", display:"block"}}><i class="fas fa-external-link-alt"></i> &nbsp;  View on Etherscan</a>
+                        </div>
+                        <div className="col-lg-4">
+                            <h5 className="mt-3 mb-5">Preview</h5>
+                            <Preview_card />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 }
--- src/components/Upload_mint.js
+++ src/components/Upload_mint.js
@@ -2,9 +2,31 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import Preview_card from "./Preview_card";
 function Upload_mint() {
+    function go_1(){
+        document.getElementById("upload_mint").style.display= "none";
+        document.getElementById("upload_mint2").style.display="block";
+        loading_time();
+    }
+    function fewSec() {
+        document.getElementById("upload_mint2").style.display = "none";
+    }
+    function fewSec2() {
+        document.getElementById("upload_mint3").style.display = "block";
+    }
+
+    function loading_time() {
+        setTimeout(() => {
+            fewSec();
+        }, 2000);
+
+        setTimeout(() => {
+            fewSec2();
+        }, 2000);
+    }
+
     return (
         <>
-            <div className="upload_mint">
+            <div className="upload_mint" id="upload_mint">
                 <div className="all_upload_info">
                     <div className="row">
                         <div className="col-lg-8">
@@ -145,6 +167,7 @@ function Upload_mint() {
                     <Button
                         variant="contained"
                         className="font-weight-light mt-5 mb-5"
+                        onClick={go_1}
                         style={{
                             backgroundColor: "#4737FF",
                             color: "#fff",
@@ -161,6 +184,85 @@ function Upload_mint() {
                     </Button>
                 </div>
             </div>
+
+
+              {/* mint your NFT */}
+
+            <div
+                className="upload_mint2"
+                id="upload_mint2"
+                style={{ display: "none" }}
+            >
+                <div className="all_upload_info">
+                    <div className="row">
+                        <div className="col-lg-8">
+                            <h1 className="fw-light mb-5">Mint your NFT</h1>
+
+                            <p className="mb-5">
+                            To continue, please confirm this transaction with your wallet. <br/>
+This will sign your wallet as the original creator of the NFT.
+                            </p>
+                            <h6>Connected to</h6>
+                            <button style={{display: "flex", alignItems:"center", justifyContent:"center", backgroundColor:"#f4f4f4", width:"158px", height:"40px", borderRadius:"30px"}}><img className="mb-0" src="/img/green_btn.png" style={{width: "10px"}}/> <p className="mb-0 ms-2">0x01a...234N</p></button>
+
+                            <img
+                                    src="/img/loadline.png"
+                                    alt=""
+                                    style={{ maxWidth: "64px" }}
+                                    className="my-4 spining_load"
+                                />
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <h5 className="mt-3 mb-5">Preview</h5>
+                            <Preview_card />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+              {/*Your NFT is now minted! */}
+
+            <div
+                className="upload_mint3"
+                id="upload_mint3"
+                style={{ display: "none" }}
+            >
+                <div className="all_upload_info">
+                    <div className="row">
+                        <div className="col-lg-8">
+                            <h1 className="fw-light mb-5">Your NFT is now minted!</h1>
+
+                            <p className="mb-5">
+                            Congratulations! Your artwork has officially been minted as an NFT on the Ethereum blockchain.</p>
+                            <Button
+                        variant="contained"
+                        className="fw-light mt-5"
+                        
+                        style={{
+                            letterSpacing: "3px",
+                            backgroundColor: "rgb(71, 55, 255)",
+                            color: "#fff",
+                            width: "241px",
+                            height: "62px",
+                            borderRadius: 0,
+                            boxShadow: "rgb(163, 185, 255) 0px 0px 1px 5px",
+                        }}
+                    >
+                        List Your NFT
+                    </Button>
+
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <h5 className="mt-3 mb-5">Preview</h5>
+                            <Preview_card />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 }
